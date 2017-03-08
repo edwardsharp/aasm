fe02b9cde3919322564d0c92e7135ccbddca696d
edward sharp <edward@edwardsharp.net>
2017-03-08 05:07:43 -0800

    scope needs to be callable (lambda)

--- lib/aasm/persistence/active_record_persistence.rb
+++ lib/aasm/persistence/active_record_persistence.rb
@@ -245,7 +245,7 @@ module AASM
           aasm_state_without_scope name, options
           
           unless self.respond_to?(name)
-            scope_options = {:conditions => { "#{table_name}.#{self.aasm_column}" => name.to_s}}
+            scope_options = lambda { where("#{table_name}.#{self.aasm_column}" => name.to_s) }
             scope_method = ActiveRecord::VERSION::MAJOR >= 3 ? :scope : :named_scope
             self.send(scope_method, name, scope_options)
           end
